33c0bfe1ff823619ba787d1c898676c85f0b256e
leksea <leksea@gmail.com>
2017-06-23 18:34:59 -0700

    Added waitTime between prelude and moving part of the stimulus

--- experiment/session/generatePrelude.m
+++ experiment/session/generatePrelude.m
@@ -8,34 +8,71 @@ function framesOut = generatePrelude(dotFrames, conditionInfo)
         pt = conditionInfo.preludeType;
     end
     
+    if ~(isfield(conditionInfo, 'waitAfterPrelude'))
+        w = .1;
+    else
+        w = conditionInfo.waitAfterPrelude;
+    end
+    
+    waitAfterPreludeFrames = w*conditionInfo.dotUpdateHz;
+    
     switch pt
-        case 'none'
-            % nothing is changed 
-%             for n = 1:nDotStructs
-%                 dotFrames{n}.L.x(1:preludeDurationFrames, :) = 0;
-%                 dotFrames{n}.L.y(1:preludeDurationFrames, :) = 0;
-%                 dotFrames{n}.R.x(1:preludeDurationFrames, :) = 0;
-%                 dotFrames{n}.R.y(1:preludeDurationFrames, :) = 0;               
-%             end
+        case 'empty'
+            %% display empty preludeDurationFrames frames in L&R eyes                        
+            for n = 1:nDotStructs                
+                dotFrames{n}.L.x(1:preludeDurationFrames, :) = NaN;
+                dotFrames{n}.L.y(1:preludeDurationFrames, :) = NaN;
+                dotFrames{n}.R.x(1:preludeDurationFrames, :) = NaN;
+                dotFrames{n}.R.y(1:preludeDurationFrames, :) = NaN;
+                
+            end
+
         case 'static'
+            %% display same first frame in L&R eyes            
             for n = 1:nDotStructs
-                %display same frame in L& R eyes
                 firstFrameX = repmat(dotFrames{n}.L.x(1, :),  [preludeDurationFrames 1]);
                 firstFrameY = repmat(dotFrames{n}.L.y(1, :),  [preludeDurationFrames 1]);
                 
                 dotFrames{n}.L.x(1:preludeDurationFrames, :) = firstFrameX;
                 dotFrames{n}.L.y(1:preludeDurationFrames, :) = firstFrameY;
                 dotFrames{n}.R.x(1:preludeDurationFrames, :) = firstFrameX;
-                dotFrames{n}.R.y(1:preludeDurationFrames, :) = firstFrameY;               
+                dotFrames{n}.R.y(1:preludeDurationFrames, :) = firstFrameY;
+                
             end
         case 'dynamic'
+            %% display same preludeDurationFrames frames in L&R eyes            
             for n = 1:nDotStructs            
                 dotFrames{n}.L.x(1:preludeDurationFrames, :) = ...
                     dotFrames{n}.R.x(1:preludeDurationFrames, :);
                 dotFrames{n}.L.y(1:preludeDurationFrames, :) = ...
                     dotFrames{n}.R.y(1:preludeDurationFrames, :);
+                
             end
     end
     framesOut = dotFrames;
     
+    if (w > 0)
+        for n = 1:nDotStructs
+            framesOut{n} = mkGapFrames(dotFrames{n}, preludeDurationFrames, waitAfterPreludeFrames);
+        end
+    end
+end
+%% Wait Frames
+function out = mkGapFrames(dotFrames, preludeDurationFrames, waitAfterPreludeFrames)
+    nLeftDots = size(dotFrames.L.x, 2);
+    nRightDots = size(dotFrames.R.x, 2);
+
+    gapFramesL = NaN*ones(waitAfterPreludeFrames, nLeftDots);
+    gapFramesR = NaN*ones(waitAfterPreludeFrames, nRightDots);
+    
+    
+    out.L.x = cat(1, dotFrames.L.x(1:preludeDurationFrames, :), gapFramesL, ...
+        dotFrames.L.x(1 + preludeDurationFrames:end, :));  
+    out.L.y = cat(1, dotFrames.L.y(1:preludeDurationFrames, :), gapFramesL, ...
+        dotFrames.L.y(1 + preludeDurationFrames:end, :));
+    
+    out.R.x = cat(1, dotFrames.R.x(1:preludeDurationFrames, :), gapFramesR, ...
+        dotFrames.R.x(1 + preludeDurationFrames:end, :));
+    out.R.y = cat(1, dotFrames.R.y(1:preludeDurationFrames, :), gapFramesR, ...
+        dotFrames.R.y(1 + preludeDurationFrames:end, :));
 end
